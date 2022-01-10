00544140ed504c7f823e1a711b6d94d6bc2a13fe
Leonardo Kunkel <mr_kunkel@hotmail.com>
2022-01-10 12:57:14 -0600

    Base

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log('Hola Mundo desde JavaScript');
+
+// VARIABLES
+// PRIMITIVOS
+// var
+var nombre = 'Hola soy Leo';
+console.log(nombre);
+
+// let
+// Forma de crear variables pero el cual su contenido puede ser mutable. Puede cambiar.
+let saludo = "Hola, cómo estás?";
+saludo = "How are you?";
+
+// const
+// Forma de crear variables, pero el contenido va a ser estático y fijo. No es mutable
+const iva = 16;
+iva = 10;
+
+// Tipos de datos
+// string
+// number
+// boolean
+// undefined - no está definido aún
+// null - ya se definió y es un valior inexistente. Ausencia de valor
+
+
+// NO PRIMITIVOS
+const nombre = "Leonardo Bravo";
+const edad = 29;
+const isIronhacker = true;
+const gonnaGetJob = undefined;
+const hasACar = null;
